5cce444f7c8d3543071c7a95b0d891f348f6ea2e
Shubhangi555 <bhosaleshubhangi333@gmail.com>
2025-07-24 17:47:49 +0530

    Add files via upload

---
+++ Section2/arrow-fun.js
@@ -0,0 +1,29 @@
+
+// syntax:
+
+// () => expression
+
+// param => expression
+
+// (param) => expression
+
+// (param1, paramN) => expression
+
+// () => {
+//   statements
+// }
+
+// param => {
+//   statements
+// }
+
+// (param1, paramN) => {
+//   statements
+// }
+
+
+const fruits= ['apple', 'banana', 'cherry'];
+
+const fruitslength=fruits.map((fruits)=> fruits.length);
+
+console.log(fruitslength)
---
+++ Section2/block.js
@@ -0,0 +1,10 @@
+var x= 1;
+let y =1;
+
+if (true) {
+    var x= 2;
+    let y =2; 
+}
+console.log(x); //2 
+console.log(y); //1
+
